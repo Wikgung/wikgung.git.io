84048db11ffdb859e00b5ff464472439ce74969e
Wikgung <96527294+Wikgung@users.noreply.github.com>
2022-02-23 10:42:06 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://wikgung.com/';
+    public $baseURL = 'https://wikgung.github.io/wikgung.git.io/';
 
     /**
      * --------------------------------------------------------------------------
